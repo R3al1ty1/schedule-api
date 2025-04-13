3b43bd16a83bd40fe9157c24677d60fe8d7600b8
R3al1ty1 <dolfinikkonovalov@gmail.com>
2025-04-13 08:29:55 +0300

    fixed cors

--- src/main.py
+++ src/main.py
@@ -30,10 +30,19 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://web.telegram.org",  # Домен Telegram Web App
+        "*",  # Для тестов можно разрешить все, но потом убери
+    ],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Разреши все нужные методы
+    allow_headers=[
+        "user-id",  # Явно разреши твой кастомный заголовок
+        "accept",
+        "content-type",
+        "authorization",
+        "cache-control",
+    ],
 )
 
 
