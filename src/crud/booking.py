86d16125baa652fdd459faa909707956bbb8d106
R3al1ty1 <dolfinikkonovalov@gmail.com>
2025-04-13 04:36:21 +0300

    fixed response

--- src/crud/booking.py
+++ src/crud/booking.py
@@ -1,25 +1,24 @@
-from typing import List
+from typing import List, Optional
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import booking as booking_model
+from core.schemas import booking as booking_schema
 
 
 async def get_bookings_db(
     db: AsyncSession,
     is_admin: bool = False,
-    user_id: int = None
-) -> List[booking_model.Booking]:
+    user_id: Optional[int] = None
+) -> List[booking_schema.Booking]:
     if is_admin and not user_id:
         stmt = select(booking_model.Booking)
-        result = await db.execute(stmt)
-        bookings = result.scalars().all()
-        return bookings
-    
-    if user_id:
+    elif user_id:
         stmt = select(booking_model.Booking).where(booking_model.Booking.user_id == user_id)
-        result = await db.execute(stmt)
-        bookings = result.scalars().all()
-        return bookings
+    else:
+        return []
+
+    result = await db.execute(stmt)
+    bookings = result.scalars().all()
     
-    return []
+    return [booking_schema.Booking.model_validate(booking) for booking in bookings]
